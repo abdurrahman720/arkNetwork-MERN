06b25188df4d87104168064595036f29d46673c3
abdurrahman720 <personal.abdurrahman@gmail.com>
2023-01-19 23:48:45 +0600

    navbar initialized

--- src/App.js
+++ src/App.js
@@ -10,16 +10,20 @@ import { themeSettings } from "./theme";
 
 function App() {
   const mode  = useSelector((state)=>state.mode)
-  const theme = useMemo(()=>createTheme(themeSettings(mode)))
+  const theme = useMemo(() => createTheme(themeSettings(mode)), [mode]);
+  
 
   return (
     <div className="app">
       <BrowserRouter>
-        <Routes>
+        <ThemeProvider theme={theme}>
+          <CssBaseline/>
+    <Routes>
           <Route path="/" element={<LoginPage/>}></Route>
           <Route path="/home" element={<HomePage/>}></Route>
           <Route path="/profile/:userId" element={<ProfilePage/>}></Route>
         </Routes>
+  </ThemeProvider>
       </BrowserRouter>
     </div>
   );
