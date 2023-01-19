dc81085c30643529b53f4509731dee079039ea72
abdurrahman720 <personal.abdurrahman@gmail.com>
2023-01-19 13:47:08 +0600

    folder structer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "./scenes/HomePage/HomePage";
+import LoginPage from "./scenes/LoginPage/LoginPage";
+import ProfilePage from "./scenes/ProfilePage/ProfilePage";
 
 
 function App() {
   return (
     <div className="app">
-      
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<LoginPage/>}></Route>
+          <Route path="/home" element={<HomePage/>}></Route>
+          <Route path="/profile/:userId" element={<ProfilePage/>}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
