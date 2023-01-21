e695372087208af1ddf5ae57f5d9968d500fec2c
abdurrahman720 <personal.abdurrahman@gmail.com>
2023-01-21 13:00:15 +0600

    navbar

--- src/scenes/HomePage/HomePage.jsx
+++ src/scenes/HomePage/HomePage.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
+import Navbar from '../Navbar/Navbar';
 
 const HomePage = () => {
     return (
         <div>
-            
+            <Navbar></Navbar>
         </div>
     );
 };
