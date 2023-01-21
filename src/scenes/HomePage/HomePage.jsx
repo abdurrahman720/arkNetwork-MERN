d617428f5a3fcf162355eed95ebcdd443b8a8ea7
abdurrahman720 <personal.abdurrahman@gmail.com>
2023-01-22 02:15:54 +0600

    user widget completed

--- src/scenes/HomePage/HomePage.jsx
+++ src/scenes/HomePage/HomePage.jsx
@@ -1,11 +1,34 @@
+import { Box, useMediaQuery } from '@mui/material';
 import React from 'react';
+import { useSelector } from 'react-redux';
 import Navbar from '../Navbar/Navbar';
+import UserWidgets from '../widgets/UserWidgets';
 
 const HomePage = () => {
+    const isNonMobileScreen = useMediaQuery("(min-width:1000px)");
+    const { _id, picturePath } = useSelector((state) => state.user);
+    
     return (
-        <div>
+        <Box>
             <Navbar></Navbar>
-        </div>
+            <Box
+                width="100%"
+                padding="2rem 6%"
+                display={isNonMobileScreen ? "flex" : "block"}
+                gap="0.5rem"
+                justifyContent="space-between"
+            >
+                <Box flexBasis={isNonMobileScreen ? "26%" : undefined}>
+                    <UserWidgets userId={_id} picturePath={picturePath}></UserWidgets>
+                </Box>
+                <Box
+                    flexBasis={isNonMobileScreen ? "42%" : undefined}
+                    mt={isNonMobileScreen ? undefined: "2rem"}
+                >
+                </Box>
+                {isNonMobileScreen && <Box flexBasis="26%"></Box>}
+            </Box>
+        </Box>
     );
 };
 
--- src/scenes/widgets/UserWidgets.jsx
+++ src/scenes/widgets/UserWidgets.jsx
@@ -1,4 +1,4 @@
-import { ManageAccountsOutlined } from '@mui/icons-material';
+import { EditOutlined, LocationOnOutlined, ManageAccountsOutlined, WorkOutlineOutlined } from '@mui/icons-material';
 import { Box, Divider, Typography, useTheme } from '@mui/material';
 import React from 'react';
 import { useEffect } from 'react';
@@ -8,7 +8,8 @@ import { useNavigate } from 'react-router-dom';
 import FlexBetween from '../../components/FlexBetween';
 import UserImage from '../../components/UserImage';
 import WidgetWrapper from '../../components/WidgetWrapper';
-
+import twitter from '../../assets/twitter.png'
+import linkedIn from '../../assets/linkedin.png'
 
 
 const UserWidgets = ({ userId, picturePath }) => {
@@ -65,12 +66,83 @@ const UserWidgets = ({ userId, picturePath }) => {
                             {friends.length} friends
                         </Typography>
                     </Box>
-                 <ManageAccountsOutlined/>
+                 
+                </FlexBetween>
+                <ManageAccountsOutlined/>
                 </FlexBetween>
                 <Divider />
+                {/* second row */}
+                <Box p="1rem 0">
+                    <Box display="flex" alignItems="center" gap="1rem" mb="0.5rem">
+                        <LocationOnOutlined fontSize='large' sx={{ color: main }} />
+                        <Typography color={medium}>
+                            {location}
+                        </Typography>
+                        </Box>
+                    <Box display="flex" alignItems="center" gap="1rem">
+                        <WorkOutlineOutlined fontSize='large' sx={{ color: main }} />
+                        <Typography color={medium}>
+                            {occupation}
+                        </Typography>
+                        </Box>
+            </Box>
+            <Divider />
+                {/* third row */}
+                <Box p="1rem 0">
+                    <FlexBetween mb="0.5rem">
+                        <Typography color={medium}>
+                            Who's viewed your Profile?
+                        </Typography>
+                        <Typography color={main} fontWeight="500">
+                           {viewedProfile}
+                        </Typography>
+                    </FlexBetween>
+                    <FlexBetween>
+                    <Typography color={medium}>
+                           Impressions on your post
+                        </Typography>
+                        <Typography color={main} fontWeight="500">
+                           {impressions}
+                        </Typography>
+                    </FlexBetween>
+                </Box>
+                <Divider />
+                {/* fourth row */}
+                <Box p="1rem 0">
+                    <Typography fontSize="1rem" color={main} fontWeight="500" mb="1rem">
+                        Social Profiles
+                    </Typography>
+                    <FlexBetween gap="1rem" mb="0.5rem">
+                        <FlexBetween gap="1rem">
+                            <img src={twitter} alt="social" />
+                            <Box>
+                            <Typography color={main} fontWeight="500">
+                                Twitter
+                            </Typography>
+                            <Typography color={medium}>
+                                Social Network
+                            </Typography>
+                           </Box>
+                        </FlexBetween>
+                        <EditOutlined sx={{color: main}}/>
+                    </FlexBetween>
 
-                
-            </FlexBetween>
+                    <FlexBetween gap="1rem" >
+                        <FlexBetween gap="1rem">
+                            <img src={linkedIn} alt="social" />
+                            <Box>
+                            <Typography color={main} fontWeight="500">
+                                Linked In
+                            </Typography>
+                            <Typography color={medium}>
+                                NetWork Platform
+                            </Typography>
+                           </Box>
+                        </FlexBetween>
+                        <EditOutlined sx={{color: main}}/>
+                    </FlexBetween>
+                </Box>
+        
         </WidgetWrapper>
     )
 };
