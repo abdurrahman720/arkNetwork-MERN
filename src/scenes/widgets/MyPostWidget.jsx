39c23794280b1975f99704667b9cbcab8413f89e
abdurrahman720 <personal.abdurrahman@gmail.com>
2023-01-25 01:54:55 +0600

    image dropzone

--- src/scenes/widgets/MyPostWidget.jsx
+++ src/scenes/widgets/MyPostWidget.jsx
@@ -1,43 +1,106 @@
-import { useMediaQuery, useTheme } from '@mui/material';
-import React, { useState } from 'react';
-import { useDispatch, useSelector } from 'react-redux';
-import setPosts from '../../state';
+import { DeleteOutlined, EditOutlined } from "@mui/icons-material";
+import { Box, IconButton, InputBase, Typography, useMediaQuery, useTheme } from "@mui/material";
+import React, { useState } from "react";
+import Dropzone from "react-dropzone";
+import { useDispatch, useSelector } from "react-redux";
+import FlexBetween from "../../components/FlexBetween";
+import UserImage from "../../components/UserImage";
+import WidgetWrapper from "../../components/WidgetWrapper";
+import setPosts from "../../state";
 
 const MyPostWidget = ({ picturePath }) => {
-    const dispatch = useDispatch();
-    const [isImage, setIsImage] = useState(false);
-    const [image, setImage] = useState(null);
-    const [post, setPost] = useState("");
-    const { palette } = useTheme();
-    const { _id } = useSelector((state) => state.user);
-    const token = useSelector((state) => state.token);
-    const isNonMobileScreen = useMediaQuery("(min-width: 1000px)")
-    const mediumMain = palette.neutral.medium;
+  const dispatch = useDispatch();
+  const [isImage, setIsImage] = useState(true);
+  const [image, setImage] = useState(null);
+  const [post, setPost] = useState("");
+  const { palette } = useTheme();
+  const { _id } = useSelector((state) => state.user);
+  const token = useSelector((state) => state.token);
+  const isNonMobileScreen = useMediaQuery("(min-width: 1000px)");
+  const mediumMain = palette.neutral.medium;
 
-    const handlePost = async () => {
-        const formData = new FormData();
-        formData.append("userId", _id);
-        formData.append("description", post);
-        if (image) {
-            formData.append("picture", image);
-            formData.append("picturePath",image.name)
-        }
-        const response = await fetch(`http://localhost:5003/posts`,{
-            method: "POST",
-            headers: { Authorization: `Bearer ${token}` },
-            body: formData
-        }
-        )
-        const posts = await response.json();
-        dispatch(setPosts({ posts }));
-        setPost("");
+  const handlePost = async () => {
+    const formData = new FormData();
+    formData.append("userId", _id);
+    formData.append("description", post);
+    if (image) {
+      formData.append("picture", image);
+      formData.append("picturePath", image.name);
     }
+    const response = await fetch(`http://localhost:5003/posts`, {
+      method: "POST",
+      headers: { Authorization: `Bearer ${token}` },
+      body: formData,
+    });
+    const posts = await response.json();
+    dispatch(setPosts({ posts }));
+    setPost("");
+  };
 
-    return (
-        <div>
-            
-        </div>
-    );
+  return (
+    <WidgetWrapper>
+      <FlexBetween gap="1.5rem">
+        <UserImage image={picturePath} />
+        <InputBase
+          placeholder="What's on your mind?"
+          onChange={(e) => setPost(e.target.value)}
+          value={post}
+          sx={{
+            width: "100%",
+            backgroundColor: palette.neutral.light,
+            borderRadius: "2rem",
+            padding: "1rem",
+          }}
+        />
+      </FlexBetween>
+      {isImage && (
+        <Box
+          border={`1px solid ${mediumMain}`}
+          borderRadius="5px"
+          mt="1rem"
+          p="1rem"
+        >
+          <Dropzone
+                    acceptedFiles=".jpg,.jpeg,.png"
+                    multiple={false}
+                    onDrop={(acceptedFiles) =>
+                        setImage(acceptedFiles[0])
+                    }
+                  >
+                      {({ getRootProps, getInputProps }) => (
+                          <FlexBetween>
+                              <Box
+                        {...getRootProps()}
+                        border={`2px dashed ${palette.primary.main}`}
+                                  p="1rem"
+                                  width="100%"
+                        sx={{ "&:hover": { cursor: "pointer" } }}
+                      >
+                        <input {...getInputProps()} />
+                        {!image ? (
+                          <p>Add Picture Here</p>
+                        ) : (
+                          <FlexBetween>
+                            <Typography>{image.name}</Typography>
+                            <EditOutlined />
+                          </FlexBetween>
+                        )}
+                              </Box>
+                              {image && (
+                                  <IconButton
+                                  onClick={()=>setImage(null)}
+                                  >
+                                      <DeleteOutlined/>
+                                  </IconButton>
+                              )}
+                          </FlexBetween>
+                      
+                    )}
+                  </Dropzone>
+        </Box>
+      )}
+    </WidgetWrapper>
+  );
 };
 
 export default MyPostWidget;
