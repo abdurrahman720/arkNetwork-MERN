da43610def86bac71a803e60ae55c7db33fc0197
abdurrahman720 <personal.abdurrahman@gmail.com>
2023-01-25 00:36:44 +0600

    handle post function

--- src/scenes/widgets/MyPostWidget.jsx
+++ src/scenes/widgets/MyPostWidget.jsx
@@ -1,6 +1,7 @@
 import { useMediaQuery, useTheme } from '@mui/material';
 import React, { useState } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import setPosts from '../../state';
 
 const MyPostWidget = ({ picturePath }) => {
     const dispatch = useDispatch();
@@ -13,7 +14,25 @@ const MyPostWidget = ({ picturePath }) => {
     const isNonMobileScreen = useMediaQuery("(min-width: 1000px)")
     const mediumMain = palette.neutral.medium;
 
-    
+    const handlePost = async () => {
+        const formData = new FormData();
+        formData.append("userId", _id);
+        formData.append("description", post);
+        if (image) {
+            formData.append("picture", image);
+            formData.append("picturePath",image.name)
+        }
+        const response = await fetch(`http://localhost:5003/posts`,{
+            method: "POST",
+            headers: { Authorization: `Bearer ${token}` },
+            body: formData
+        }
+        )
+        const posts = await response.json();
+        dispatch(setPosts({ posts }));
+        setPost("");
+    }
+
     return (
         <div>
             
