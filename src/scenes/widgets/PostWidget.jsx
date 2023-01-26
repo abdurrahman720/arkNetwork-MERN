bb70ece89cfa1ae0ca71c31dc3018e284787b37a
abdurrahman720 <personal.abdurrahman@gmail.com>
2023-01-26 16:33:32 +0600

    patch like function

--- src/scenes/widgets/PostWidget.jsx
+++ src/scenes/widgets/PostWidget.jsx
@@ -1,7 +1,33 @@
-import React from 'react';
+import { useTheme } from '@mui/material';
+import React, { useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 
 const PostWidget = ({ post }) => {
     const { _id, userId, firstName, lastName, description, location, picturePath, userPicturePath, likes, comments } = post;
+
+    const [isComment, setIsComment] = useState(false);
+    const loggedInUserId = useSelector((state) => state.user._id) //currently logged in user 
+    const isLiked = Boolean(likes[loggedInUserId]); // we need to check if this user already liked it or not
+    const likedCount = Object.keys(likes).length; // likes is not an array rather a map
+
+    const dispatch = useDispatch();
+    const token = useSelector((state) => state.token);
+
+
+    const { palette } = useTheme();
+    const primary = palette.primary.main;
+    const main = palette.neutral.main;
+
+    const patchLike = async () => {
+        const res = await fetch(`http://localhost:5003/posts/${_id}/like`, {
+            method: "PATCH",
+            headers: {
+                Authorization: `Bearer ${token}`,
+                "Content-type": "application/json"
+            },
+            body: JSON.stringify({userId: loggedInUserId})
+        })
+    }
     
     return (
         <div>
